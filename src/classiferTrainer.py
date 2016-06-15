2f8c5e703265fd667738b3a41109d5ef047f1ce1
Jesse Earisman <jearism@us.ibm.com>
2016-06-15 15:03:42 -0400

    Adjusted redditDataGetter to get more data

--- src/classiferTrainer.py
+++ src/classiferTrainer.py
@@ -1,6 +1,12 @@
 import json
 import time
 from watson_developer_cloud import VisualRecognitionV3
+#from visual_recognition_v3 import VisualRecognitionV3
+import requests
+
+
+
+apikey = 'cb413d69a797e117154d8474ea7397a04866551a'
 
 # A class to analyze tone, some sort of ... Tone Analyzer
 # Really only handles emotion data
@@ -10,7 +16,13 @@ class VisualTrainer:
 	
 	def __init__(self):
 		# TODO
-		visual_recognition = VisualRecognitionV3('2016-05-20', api_key='')
+		"""
+		self.visual_recognition = VisualRecognitionV3(
+			password = "awDho4FkDovI",
+        	username = "b9917582-7ff3-49a4-9104-aae4b64035b2",
+			version  = "2016-05-20")
+		"""
+		self.visual_recognition = VisualRecognitionV3('2016-05-20', api_key=apikey)
 	
 	# Build a new classifier with the given name.
 	# Name could be something like "Cats vs Dogs"
@@ -22,8 +34,7 @@ class VisualTrainer:
 	def new_classifier( self, classifier_name, class_names, zipfiles, neg_zipfile ):
 		print( "Are you sure you want to create a new classifier" )
 		print( "Current classifier list is:" )
-		#print( visual_recognition.list_classifiers( verbose= True ) )
-		text = input( "Type 'YES' to continue creating classifier {}: ".format( classifier_name ) )
+		text = input( "Type 'YES' to continue creating classifier \"{}\": ".format( classifier_name ) )
 		if( text != "YES" ):
 			print( "exiting, no classifier created" )
 			return None
@@ -32,6 +43,10 @@ class VisualTrainer:
 		n_classes = len( class_names )
 		arguments_str = ""
 		open_files = []
+		# This is a little weird, basically, we don't know the names or how many classes we want
+		# we need to generate the parameter names on the fly and build the exec string
+		# while maintaining a list of open files, so that we can close them later
+		# I wouldn't touch this if I were you
 		for i in range( 0, n_classes ):
 			zipf_name = class_names[i] + "_zip"
 			exec( "{} = open( \"{}\", \"rb\")".format( zipf_name, zipfiles[i] ))
@@ -42,23 +57,41 @@ class VisualTrainer:
 		open_files.append( neg_zip )
 		arguments_str += "negative_examples=neg_zip"
 		exec_str = "ret_json = self.visual_recognition.create_classifier( \"{}\", {} )".format( classifier_name, arguments_str)
-		print( exec_str )
-		# exec( exec_str ) )
+		#print( exec_str )
+		exec( exec_str )
 		for f in open_files:
 			f.close()
-		#return ret_json
+		if( ret_json is not None ):
+			print( json.dumps( ret_json, indent=2) )
+			return ret_json
+
+	def list_classifiers(self):
+		print( json.dumps( self.visual_recognition.list_classifiers( verbose= True ), indent=4) )
+
+	def del_classifier( self, classifier_id ):
+			self.visual_recognition.delete_classifier( classifier_id)
+		
+
+	def classify_single_image( self, url, classifier_id = None ):
+		return self.visual_recognition.classify( images_url= url, classifier_ids=classifier_id )
+
+		
 
 
 def main():
 	v = VisualTrainer()
-	v.new_classifier( 
-			"Dogs vs Cats", 
-			["dog", "cat"], 
-			["dogs.zip", "cat_ex.zip"], 
-			"meese.zip")
+
+	url = "http://i.imgur.com/Yq1mc9H.jpg"
+	v.list_classifiers()
+	print( 
+			json.dumps(
+				v.visual_recognition.classify( 
+					images_url=url, 
+					classifier_ids="Angerornot_1582529321",
+					owners=apikey),
+				indent=2 ) )
+
+
 
 if( __name__ == "__main__"):
 	main()
-
-
-
