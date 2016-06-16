f4da242fb54df466f25dc5dccd1505bdc18afa8c
Jesse Earisman <jearism@us.ibm.com>
2016-06-16 14:42:23 -0400

    Re-added good classifier

--- src/classifier.py
+++ src/classifier.py
@@ -3,26 +3,28 @@ import time
 from watson_developer_cloud import VisualRecognitionV3
 #from visual_recognition_v3 import VisualRecognitionV3
 import requests
+import sys
 
 
 
 apikey = 'cb413d69a797e117154d8474ea7397a04866551a'
+anger_class_id ="Angerornotv2_2085176946"
 
 # A class to analyze tone, some sort of ... Tone Analyzer
 # Really only handles emotion data
 class VisualTrainer:
 
-	visual_recognition = None
+	v = None
 	
 	def __init__(self):
 		# TODO
 		"""
-		self.visual_recognition = VisualRecognitionV3(
+		self.v = VisualRecognitionV3(
 			password = "awDho4FkDovI",
         	username = "b9917582-7ff3-49a4-9104-aae4b64035b2",
 			version  = "2016-05-20")
 		"""
-		self.visual_recognition = VisualRecognitionV3('2016-05-20', api_key=apikey)
+		self.v = VisualRecognitionV3('2016-05-20', api_key=apikey)
 	
 	# Build a new classifier with the given name.
 	# Name could be something like "Cats vs Dogs"
@@ -34,6 +36,7 @@ class VisualTrainer:
 	def new_classifier( self, classifier_name, class_names, zipfiles, neg_zipfile ):
 		print( "Are you sure you want to create a new classifier" )
 		print( "Current classifier list is:" )
+		self.list_classifiers()
 		text = input( "Type 'YES' to continue creating classifier \"{}\": ".format( classifier_name ) )
 		if( text != "YES" ):
 			print( "exiting, no classifier created" )
@@ -52,46 +55,93 @@ class VisualTrainer:
 			exec( "{} = open( \"{}\", \"rb\")".format( zipf_name, zipfiles[i] ))
 			exec( "open_files.append( {} )".format( zipf_name ) )
 			arguments_str += "{}_positive_examples={}, ".format( class_names[i], zipf_name )
+			print( arguments_str )
 
 		neg_zip = open( neg_zipfile, "rb" )
 		open_files.append( neg_zip )
 		arguments_str += "negative_examples=neg_zip"
-		exec_str = "ret_json = self.visual_recognition.create_classifier( \"{}\", {} )".format( classifier_name, arguments_str)
-		#print( exec_str )
+		exec_str = "ret_json = self.v.create_classifier( \"{}\", {} )".format( classifier_name, arguments_str)
+		print( exec_str )
 		exec( exec_str )
 		for f in open_files:
 			f.close()
-		if( ret_json is not None ):
-			print( json.dumps( ret_json, indent=2) )
-			return ret_json
+		return
 
 	def list_classifiers(self):
-		print( json.dumps( self.visual_recognition.list_classifiers( verbose= True ), indent=4) )
+		print( json.dumps( self.v.list_classifiers( verbose= True ), indent=4) )
 
 	def del_classifier( self, classifier_id ):
-			self.visual_recognition.delete_classifier( classifier_id)
+			self.v.delete_classifier( classifier_id)
 		
 
-	def classify_single_image( self, url, classifier_id = None ):
-		return self.visual_recognition.classify( images_url= url, classifier_ids=classifier_id )
+	def classify_single_image( self, url, classifier_ids = None ):
+		to_ret = self.v.classify( images_url = url, classifier_ids = self.get_classifier_ids(), threshold = 0.0)
+		#print( json.dumps( to_ret, indent=2 ) )
+		self.pp_classify_response( to_ret )
+		return to_ret
 
-		
+	def pp_classify_response( self, some_json):
+		for i in some_json["images"]:
+			for c in i["classifiers"]:
+				for cl in c["classes"]:
+					print("{} : {}".format( cl["class"], cl["score"] ) )
+
+	def del_all_classifiers( self ):
+		self.list_classifiers()
+		text = input( "This will delete ALL classifiers. Type \"YES\" if you really want to do this: ")
+		if( text != "YES" ):
+			print( "exiting, nothing deleted created" )
+			return None
+		class_list = self.v.list_classifiers( verbose = True )
+		for c in class_list["classifiers"]:
+			self.v.delete_classifier( c["classifier_id"] )
+
+	def get_classifier_ids( self ):
+		to_ret = []
+		class_list = self.v.list_classifiers( verbose = True )
+		for c in class_list["classifiers"]:
+			to_ret.append( c["classifier_id"] )
+		return to_ret
+
+	# Start from the ground up, nuke all classifiers and rebuild them.
+	# This should only be called when the data has been updated, for instance
+	def rebuild_classifiers( self ):
+		self.del_aall_classifiers()
+		vis.new_classifier(
+				"angerclass",
+				["anger"],
+				["../data/anger.zip"],
+				"../data/neg-anger.zip")
+		vis.new_classifier(
+				"disgustclass",
+				["disgust"],
+				["../data/disgust.zip"],
+				"../data/neg-disgust.zip")
+		vis.new_classifier(
+				"fearclass",
+				["fear"],
+				["../data/fear.zip"],
+				"../data/neg-fear.zip")
+		vis.new_classifier(
+				"joyclass",
+				["joy"],
+				["../data/joy.zip"],
+				"../data/neg-joy.zip")
+		vis.new_classifier(
+				"sadnessclass",
+				["sadness"],
+				["../data/sadness.zip"],
+				"../data/neg-sadness.zip")
 
 
-def main():
-	v = VisualTrainer()
 
-	url = "http://i.imgur.com/Yq1mc9H.jpg"
-	v.list_classifiers()
-	print( 
-			json.dumps(
-				v.visual_recognition.classify( 
-					images_url=url, 
-					classifier_ids="Angerornot_1582529321",
-					owners=apikey),
-				indent=2 ) )
+#v.new_classifier("Anger or not v2", ["anger"], ["../data/anger.zip"], "../data/neg-anger.zip")
 
+def main( args ):
 
+	vis = VisualTrainer()
+	for a in args:
+		vis.classify_single_image(a)
 
-if( __name__ == "__main__"):
-	main()
+if( __name__ == "__main__" ):
+	main( sys.argv[1:] )
