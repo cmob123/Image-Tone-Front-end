0b3f15a64a5085dba38df6bea2c5690025f4dfff
Jesse Earisman <jearism@us.ibm.com>
2016-06-24 13:55:10 -0400

    Made changes to redditDataSaver to get more than 1000 posts worth of data

--- src/redditDataSaver.py
+++ src/redditDataSaver.py
@@ -3,8 +3,9 @@ import json
 import time
 import sys
 import csv
+import time
 import random
-from pprint import pprint
+import math
 from tone import ToneAnalyzer
 
 # Pulls in the top posts to r/pics
@@ -15,84 +16,106 @@ from tone import ToneAnalyzer
 fieldnames = ['name', 'arbitrary_id', 'url', 'title', 'title_data', 'comments', 'comment_data']
 
 def main():
-	save_submissions( n_posts = 10000, n_comments = 25, data_dir = "../data/", data_fn = "data.csv")
+	save_submissions( n_posts = 2048, n_comments = 25, data_dir = "../data/", train_fn = "train.csv", test_fn = "test.csv")
 
 
 def save_submissions( n_posts, n_comments = 25, data_dir = "../data/", train_fn = "train.csv", test_fn = "test.csv" ):
 	print( "Retrieving {} data points from r/pics".format( n_posts ) )
-	agent = praw.Reddit( user_agent='Comment-picture associator' )
 # I'm not sure why these flush() calls are needed, but if they aren't there, nothing gets printed until the end
 	sys.stdout.flush()
 
 	# change to a if we want to append instead of overwrite
-	train_file = open(data_dir + train_fn, "w", newline='' )
-	train_csv_writer = csv.DictWriter( train_file, fieldnames=fieldnames)
-	train_csv_writer.writeheader()
-	test_file = open(data_dir + test_fn, "w", newline='' )
-	test_csv_writer = csv.DictWriter( test_file, fieldnames=fieldnames)
-	test_csv_writer.writeheader()
+	f_train = open(data_dir + train_fn, "w", newline='' )
+	csvw_train = csv.DictWriter( f_train, fieldnames=fieldnames)
+	csvw_train.writeheader()
+	f_test = open(data_dir + test_fn, "w", newline='' )
+	csvw_test = csv.DictWriter( f_test, fieldnames=fieldnames)
+	csvw_test.writeheader()
 	t = ToneAnalyzer()
-	records_count = 0
-	posts_count = 0
-	last_post = None
-	after = None
+	c_records = 0
+	c_posts = 0
+	# Current timestamp
+	ts_now = int(time.time())
+	# timestamp from 2011-1-1 @ 00:00:00
+	ts_first = 1293840000
+	n_max_step_posts = 500
+	n_steps = int( math.ceil( n_posts / n_max_step_posts ) )
+	ts_step_size = (ts_now - ts_first) / n_steps
+	print("first timestamp is {}".format(ts_first))
+	print("Currently it is {}".format(ts_now))
 
-	while True:
-		submissions = agent.get_subreddit( 'pics' ).get_top_from_all( limit=None, params = {'after': after} )
-		# This guarantees that we will always get the same testing and training data
+	ts_step = ts_now
 
-		random.seed(0)
-		for x in submissions:
-			last_post = x
-			print( "Retriving post {}".format( posts_count ) )
-			print( x.name )
+	agent = praw.Reddit( user_agent='Comment-picture associator' )
+	submissions = agent.search( query = "", subreddit = "pics", sort="top", limit=None, syntax="cloudsearch" )
+	# This guarantees that we will always get the same testing and training data
+
+	random.seed(0)
+	while( True ):
+		c_step_posts = 0
+		str_timestamp_range = "{}..{}".format( int(ts_step - ts_step_size ), int(ts_step) )
+		print("Current timestamp range is " + str_timestamp_range)
+		print("c_posts = {}".format( c_posts ) )
+		str_search = "timestamp:" + str_timestamp_range
+		submissions = agent.search( str_search, subreddit = "pics", sort="top", limit=None, syntax="cloudsearch" )
+		ts_step -= ts_step_size
+		for sub in submissions:
+			print( "Retriving post {}".format( c_posts ) )
 			sys.stdout.flush()
-			posts_count += 1
-			# Make sure each image is a single image, not an album or a .gif
-			if( (len(x.url) < 4) or ((x.url[-4:] != ".jpg") and (x.url[-4:] != ".png"))):
-				continue
+			c_step_posts += 1
+			c_posts += 1
 
-			try:
-				comment_tree = x.comments
-				comment_concat = ""
-				num_comments = len( comment_tree ) - 1
-				if( num_comments > n_comments ):
-					num_comments = n_comments
-				for i in range( 0, num_comments ):
-					if( "body" in vars( comment_tree[i] ) ):
-						comment_concat += str( vars( comment_tree[i])["body"])
-			except:
-				print( "Error getting comments, are we being throttled by reddit?" )
-				print( sys.exc_info() )
-				if( comment_concat == "" ):
-					continue
+			#Process the image, returns true if it processed successfully
+			if( process_image( sub, t, random.choice( [csvw_train, csvw_test]), n_comments, c_records ) ):
+				c_records += 1
+				print( "Wrote record {}".format( c_records ) )
+				sys.stdout.flush()
 
-			tone = t.tone_analyze( comment_concat )
-			comment_data = t.tone_all_num_extract( tone )
-			title_tone = t.tone_analyze( x.title )
-			title_data = t.tone_all_num_extract( title_tone )
-			post_data = {'name': x.name, 'arbitrary_id': records_count, 'url': x.url, 'title': x.title, 'title_data': title_data, 'comments': comment_concat, 'comment_data': comment_data}
-				
-			try:
-				csv_writer = random.choice( [train_csv_writer, test_csv_writer] )
-
-				csv_writer.writerow( post_data )
-			except UnicodeEncodeError as e:
-				print("Unicode error, cannot save this submission, sorry")
-				continue
-			except:
-				print("Something went wrong processing this post")
-				print(sys.exc_info()[0])
-				continue
-
-			print("Wrote record {}".format( records_count) )
-			if( records_count >= n_posts ):
-				train_file.close()
-				test_file.close()
+			if( c_step_posts >= n_max_step_posts ):
+				break
+			if( c_posts >= n_posts ):
+				f_train.close()
+				f_test.close()
 				return
-			records_count += 1
-			sys.stdout.flush()
-		after = last_post.name
+
+def process_image( sub, ta, csvw_choice, n_comments, id_to_use):
+	if(len(sub.url) < 4):
+		return False
+	if( (sub.url[-4:] != ".jpg") and (sub.url[-4:] != ".png") ):
+		return False
+	try:
+		comment_tree = sub.comments
+		comment_concat = ""
+		c_post_comments = len( comment_tree ) - 1
+		if( c_post_comments > n_comments ):
+			c_post_comments = n_comments
+		for i in range( 0, c_post_comments ):
+			if( "body" in vars( comment_tree[i] ) ):
+				comment_concat += str( vars( comment_tree[i])["body"])
+	except:
+		print( "Error getting comments, are we being throttled by reddit?" )
+		print( sys.exc_info() )
+		if( comment_concat == "" ):
+			return False
+
+	tone = ta.tone_analyze( comment_concat )
+	comment_data = ta.tone_all_num_extract( tone )
+	title_tone = ta.tone_analyze( sub.title )
+	title_data = ta.tone_all_num_extract( title_tone )
+
+	post_data = {'name': sub.name, 'arbitrary_id': id_to_use, 'url': sub.url, 'title': sub.title, 'title_data': title_data, 'comments': comment_concat, 'comment_data': comment_data}
+		
+	try:
+
+		csvw_choice.writerow( post_data )
+	except UnicodeEncodeError as e:
+		print("Unicode error, cannot save this submission, sorry")
+		return False
+	except:
+		print("Something went wrong processing this post")
+		print(sys.exc_info()[0])
+		return False
+	return True
 
 if __name__ == "__main__":
     main()
