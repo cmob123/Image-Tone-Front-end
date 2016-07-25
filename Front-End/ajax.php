5722649c977b5568244d59e41ad3aaca602fe61e
CHRISTOPHER M. O'BRIEN <cmobrien@us.ibm.com>
2016-07-25 14:17:01 -0400

    Delete ajax.php

--- Front-End/ajax.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Serves as a messenger between client-side JavaScript and server-side Python. Written by Chris O'Brien
-echo exec("python Back-End/processImage.py " . $_POST["input"]); // run python script to proces image
-?>
